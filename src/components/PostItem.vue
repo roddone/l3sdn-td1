db89133e259d6cc435755c783b24400a9b0952ca
Lucas Maveau <lucas.maveau@gmail.com>
2024-02-19 17:39:38 +0100

    lucas_maveau td1

---
+++ src/components/PostItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    <strong>{{ post.username }}</strong> - {{ post.postText }} - {{ post.date }}
+    <button @click="deletePost">Delete</button>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps(['post']);
+
+const deletePost = () => {
+  emit('deletePost');
+};
+</script>

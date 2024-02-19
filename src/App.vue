893c9912ea0e2f29efffed1af00a48bf607f05f9
IziFrize <IziFrize@gmail.com>
2024-02-19 17:35:55 +0100

    Tourtelier LE PLUS BO

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
-<script setup>
-</script>
-
 <template>
 
-</template>
-
-<style scoped>
-</style>
+    <ul style="list-style-type: none; margin: 0; padding: 0;">
+        <div style="text-align: center; margin-top: 20px;">
+      <input type="text"
+             style="width: 300px; height: 40px; margin-bottom: 20px;"
+             v-model="textInput"
+             @keyup.enter="addTweet"
+             placeholder="Écrivez quelque chose et appuyez sur Entrée">
+    </div>
+      <li v-for="(tweet, index) in tweets" :key="index">
+        <Tweet :tweet="tweet" />
+      </li>
+    </ul>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import Tweet from './Tweet.vue'
+  const textInput = ref('');
+  const tweets = ref([]);
+  
+  function addTweet() {
+    if (textInput.value.trim() !== '') {
+      const now = new Date();
+      const dateTimeString = now.toLocaleString();
+  
+      tweets.value.unshift({ text: textInput.value, dateTime: dateTimeString });
+      textInput.value = '';
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ src/text-enter.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+
+  <div style="text-align: center; margin-top: -50%; margin-left:50%;">
+    <input type="text" style="width: 500px; height: 150px;">
+  </div>
+</template>
+
+<style scoped>
+</style>

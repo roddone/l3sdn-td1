eb4c861b94142ae4163fd913bede7bf741438272
harkansky <gregoire.wartner@lacatholille.fr>
2024-02-19 16:51:33 +0100

    first draft

---
+++ src/components/SearchPosts.vue
@@ -0,0 +1,11 @@
+<template>
+    <input
+        style="top: 0; left: 0; background: white; color: black; padding: 1rem; width: 100%;"
+        v-model="search"
+        placeholder="Rechercher"
+    />
+</template>
+
+<script setup>
+    let search = '';
+</script>

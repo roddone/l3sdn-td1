f5c3b6559611592974567a5b653db5753545a18b
RomeoFroger49 <romeo.froger@gmail.com>
2024-02-19 17:51:16 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup>
+import ListContainer from "./component/ListContainer.vue";
+import SearchBar from "./component/SearchBar.vue";
+import { ref } from "vue";
+
+let searchVariable = ref("")
 </script>
 
 <template>
-
+  <div class="container">
+    <list-container :search="searchVariable" />
+    <search-bar v-model="searchVariable"/>
+    <p> {{ searchVariable }}</p>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
 </style>
---
+++ src/component/Formulaire.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from "vue";
+
+let tweet = defineModel({
+  username: String,
+  content: String,
+  date: String,
+});
+
+const content = ref("");
+const username = ref("user1");
+
+const handleForm = () => {
+  const tweetForm = {
+    username: username.value,
+    content: content.value,
+    date: Date.now(),
+  };
+  
+  tweet.value = tweetForm;
+  
+  content.value = "";
+};
+</script>
+
+<template>
+  <form class="tweet">
+    <div>
+      <input
+        v-model="content"
+        type="text"
+        id="content"
+        name="content"
+        placeholder="Partagez votre avis..."
+      />
+    </div>
+    <div class="button">
+      <div @click="handleForm">Publier</div>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+.tweet {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.button {
+  margin-top: 10px;
+  border: none;
+}
+
+#content {
+  width: 100%;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #ccc;
+}
+</style>

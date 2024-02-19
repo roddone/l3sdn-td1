e61c55e6c95d4c959cb720ac958dbca296aab156
axel59830 <axel.krebs145@gmail.com>
2024-02-19 16:48:41 +0100

    axelkrebs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script setup>
+import like from './components/like.vue'
+import poster from './components/poster.vue'
+import search from './components/search.vue'
+import { ref } from 'vue'
+
+const clickcout = ref(0);
+
 </script>
 
 <template>
 
+<div><poster/></div> 
+<br/>
+<div><search/></div>
+
+<br>
+
+<div>
+    <like title="jaime" v-model="clickcout" />
+    <p>NBjaimes : {{ clickcout }}</p>
+
+</div>
+
+
+
+
+
 </template>
+
 
 <style scoped>
 </style>
---
+++ src/components/search.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+
+<template>
+<div>
+    <form id="auto-suggest" action="#" method="post">
+    <fieldset>
+    <input type="text" class="search" name="search" value="Search / Rechercher..." onfocus="if(this.value=='Search / Rechercher...')this.value=''" onblur="if(this.value=='')this.value='Search / Rechercher...'" autocomplete="off"/>
+    </fieldset>
+</form>
+</div>
+</template>

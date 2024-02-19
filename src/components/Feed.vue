1ffe2d76d5e622b399a3965a1c3c16ff2fce7857
Shinibarty <albanstievenard59@gmail.com>
2024-02-19 16:34:00 +0100

    ca avance

---
+++ src/components/Feed.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <Tweet v-for="tweet in tweets" :key="tweet.id" :tweetContent="tweet.content" :timeAgo="tweet.timeAgo" />
+    </div>
+  </template>
+  
+  <script>
+  import Tweet from './Tweet.vue';
+  
+  export default {
+    components: {
+      Tweet
+    },
+    props: ['tweets']
+  };
+  </script>
+  
---
+++ src/components/Tweet.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <p>{{ tweetContent }} - {{ timeAgo }}</p>
+      <p><button>Répondre</button><button>Retweeter</button><button>Aimer</button></p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['tweetContent', 'timeAgo']
+  };
+  </script>
+  

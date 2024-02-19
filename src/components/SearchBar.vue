f0d00d7257a469ff906d461b2d5f6e2190fcf0ca
Hanif <hanif.nmh.01@gmail.com>
2024-02-19 16:37:22 +0100

    Màj de la search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,23 +1,87 @@
 <template>
-    <div class="search-bar">
-      <input v-model="searchQuery" placeholder="Rechercher..." />
-      <button @click="search">Chercher</button>
+  <div class="search-bar">
+    <input
+      type="text"
+      v-model="searchQuery"
+      @input="filterResults"
+      @keyup.enter="confirmSelection"
+      placeholder="Rechercher..."
+      class="search-input"
+    />
+    <div class="search-results" v-show="searchQuery && (filteredAccounts.length || filteredTweets.length)">
+      <ul>
+        <li v-for="account in filteredAccounts" :key="account.id" @click="selectResult(account)">
+          {{ account.name }} - {{ account.passion }}
+        </li>
+        <li v-for="tweet in filteredTweets" :key="tweet.id" @click="selectResult(tweet)">
+          {{ tweet.content }}
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        searchQuery: ''
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      accounts: [
+        { id: 1, name: 'Abdi' , passion: 'Manga'},
+        { id: 2, name: 'Bertrand', passion: 'Sport' },
+        { id: 3, name: 'Charles', passion: 'Cinéma' },
+        { id: 4, name: 'Dounia', passion: 'Trading' },
+        { id: 5, name: 'Eric', passion: 'Dessin Animés' },
+        { id: 6, name: 'Florian', passion: 'Voitures' },
+        { id: 7, name: 'Guedid', passion: 'Motos' },
+        { id: 8, name: 'Hannah', passion: 'Marque de vêtements' },
+        { id: 9, name: 'Ismael', passion: 'Manga' },
+        { id: 10, name: 'Junior', passion: 'Sport' },
+        { id: 11, name: 'Karima',passion: 'Tennis' },
+        { id: 12, name: 'Louanne', passion: 'Manga' }
+      ],
+      tweets: [ /* ... vos tweets ... */ ],
+      filteredAccounts: [],
+      filteredTweets: [],
+      selectedResult: null,
+    };
+  },
+  methods: {
+    filterResults() {
+      this.filterAccounts();
+      this.filterTweets();
     },
-    methods: {
-      search() {
-        this.$emit('onSearch', this.searchQuery);
+    filterAccounts() {
+      if (!this.searchQuery) {
+        this.filteredAccounts = [];
+        return;
+      }
+      this.filteredAccounts = this.accounts.filter(account =>
+        account.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+    filterTweets() {
+      if (!this.searchQuery) {
+        this.filteredTweets = [];
+        return;
+      }
+      this.filteredTweets = this.tweets.filter(tweet =>
+        tweet.content.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
+    selectResult(result) {
+      this.selectedResult = result;
+      this.searchQuery = '';
+      this.filteredAccounts = [];
+      this.filteredTweets = [];
+    },
+    confirmSelection() {
+      if (this.filteredAccounts.length || this.filteredTweets.length) {
+        this.selectResult(this.filteredAccounts.length ? this.filteredAccounts[0] : this.filteredTweets[0]);
       }
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
@@ -31,7 +95,6 @@
   border-radius: 20px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
 }
-
 .search-input {
   flex-grow: 1;
   border: none;
@@ -41,33 +104,24 @@
   font-size: 1rem;
 }
 
-.search-input:focus {
-  outline: none;
-  box-shadow: 0 0 0 2px #1da1f2;
+.search-results {
+  position: absolute;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  width: 100%;
+  z-index: 1000;
 }
-
-.search-button {
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 20px;
+.search-results ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.search-results li {
+  padding: 10px;
   cursor: pointer;
-  background-color: #1da1f2;
-  color: white;
-  font-weight: bold;
-  text-transform: uppercase;
 }
-
-.search-button:hover {
-  background-color: #1a91da;
-}
-
-.search-button:active {
-  background-color: #198ccf;
-}
-
-.search-button:focus {
-  outline: none;
-  box-shadow: 0 0 0 2px #1a91da;
+.search-results li:hover {
+  background-color: #f9f9f9;
 }
 </style>
-  
